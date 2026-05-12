ac58e034947736fc05332dc4e70e64258b9baa1a
Liz <liz@ejemplo.com>
2026-05-11 20:01:29 -0500

    Adición de comentario

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeRegressor
 
 #Crear dataset
 
+# Crear modelo y validarlo 
 X, y = load_diabetes(return_X_y=True)
 regressor = DecisionTreeRegressor(random_state=0)
 cross_val_score(regressor, X, y, cv=10)
